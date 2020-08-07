78f1c2157042b4dfee1bb00f8d8643139c9db7b0
sika0115 <nh3988@gmail.com>
2020-08-07 14:18:55 +0900

    Firebaseのメールリンクログインを有効にする

---
+++ ArtworkExhibition/ArtworkExhibition/Pods/Target Support Files/Pods-ArtworkExhibition-ArtworkExhibitionUITests/Pods-ArtworkExhibition-ArtworkExhibitionUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArtworkExhibition_ArtworkExhibitionUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ArtworkExhibition_ArtworkExhibitionUITests
+@end
---
+++ ArtworkExhibition/ArtworkExhibition/Pods/Target Support Files/Pods-ArtworkExhibitionTests/Pods-ArtworkExhibitionTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ArtworkExhibitionTests : NSObject
+@end
+@implementation PodsDummy_Pods_ArtworkExhibitionTests
+@end
