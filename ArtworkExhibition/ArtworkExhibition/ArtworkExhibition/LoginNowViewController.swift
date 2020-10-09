0d2c94655b66d5b534b4ae5feb57d1ffeef35853
sika0115 <nh3988@gmail.com>
2020-10-09 13:49:37 +0900

    中間発表時の進捗

---
+++ ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/LoginNowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginNowViewController.swift
+//  ArtworkExhibition
+//
+//  Created by 庄司果鈴 on 2020/10/09.
+//  Copyright © 2020 Karin Shoji. All rights reserved.
+//
+import Firebase
+import UIKit
+
+class LoginNowViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
