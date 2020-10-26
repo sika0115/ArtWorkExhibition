acc3eb8167979d0fd75e4ee1383e3214846c6e21
sika0115 <nh3988@gmail.com>
2020-10-26 11:58:12 +0900

    画像アップロード一旦あげ

--- ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/ArtWorkUploadViewController.swift
+++ ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/ArtWorkUploadViewController.swift
@@ -15,38 +15,50 @@ class ArtWorkUploadViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // [1]ストレージ サービスへの参照を取得
-        let storage = Storage.storage()
-
-        // [2]ストレージへの参照を取得
-        let storageRef = storage.reference(forURL: "gs://artworkexhibition-a476c.appspot.com")
-
-        // [3]ツリーの下位への参照を作成
-        let imageRef = storageRef.child("images/image.jpg")
-
-        // [4]Dataを作成
-        let imageData = UIImage(named: "image")!.jpegData(compressionQuality: 1.0)!
-
-        // [5]アップロードを実行
-        imageRef.putData(imageData, metadata: nil) { metadata, error in
-            if (error != nil) {
-                print("an error occurred!")
-            } else {
-                // Metadata contains file metadata such as size, content-type.
-                let size = metadata?.size
-                  // You can also access to download URL after upload.
-                  imageRef.downloadURL { (url, error) in
-                    guard let downloadURL = url else {
-                      // Uh-oh, an error occurred!
-                      return
-              //  let downloadURL = metadata!.downloadURL()!
-              //  print("downloadURL:", downloadURL)
-                    }
-       
-                  }
-            }
-            
-            
+        let ud = UserDefaults.standard
+                ud.set(0, forKey: "count")
+        
+        func countPhoto() -> String {
+                let ud = UserDefaults.standard
+                let count = ud.object(forKey: "count") as! Int
+                ud.set(count + 1, forKey: "count")
+                return String(count)
         }
+        
+        
+        
+        //ストレージ サービスへの参照を取得
+        let storage = Storage.storage()
+        //ストレージサービスからストレージ参照を作成
+        let storageRef = storage.reference(forURL: "gs://artworkexhibition-a476c.appspot.com")
+        // ディスク上にあるファイル
+        let localFile = URL(string: "path/to/image")!
+        // 子参照を作成
+        // imagesRefが「images」を指すようになった(アップロードするファイルへの参照を作成)
+        let imagesRef = storageRef.child("image/" + NSUUID().uuidString + "/" + countPhoto() + ".jpg")
+        
+        //ファイルをパスにアップロード
+        let uploadTask = imagesRef.putFile(from: localFile, metadata: nil) { metadata, error in
+          guard let metadata = metadata else {
+            // エラー発生
+            return
+          }
+        /*//メタデータには、サイズ、コンテンツタイプなどのファイルメタデータが含まれる
+        let size = metadata.size
+        */
+        /*//アップロード後にダウンロードURLにアクセスする
+        imagesRef.downloadURL { (url, error) in
+            guard let downloadURL = url else {
+                  // エラーが発生
+                  return
+            }
+        }*/
+        
+        
+    }
+    
+
+    
+   
     }
 }
