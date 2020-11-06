d7e15dffd0f7239cad7331fccb41cb2bb280f682
sika0115 <nh3988@gmail.com>
2020-11-06 11:39:31 +0900

    extensionエラー

--- ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/ImageUploadViewController.swift
+++ ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/ImageUploadViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 import Firebase
 
 class ImageUploadViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-
+    
+    @IBOutlet weak var UploadImageView: UIImageView!{
+        didSet {
+            // デフォルトの画像を表示する
+            UploadImageView.image = UIImage(named: "no_image.png")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-    @IBOutlet weak var UploadImageView: UIImageView!
-    
-    // 画像が選択された時に呼ばれる
+    /*// 画像が選択された時に呼ばれる
        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])  {
            if let selectedImage = info[.originalImage] as? UIImage {
                UploadImageView.image = selectedImage  //imageViewにカメラロールから選んだ画像を表示する
@@ -31,15 +35,30 @@ class ImageUploadViewController: UIViewController, UIImagePickerControllerDelega
        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
            self.dismiss(animated: true, completion: nil)
        }
+    */
     
-    
-   @IBAction func UploadImageButton(_ sender: Any) {
-        let picker = UIImagePickerController() //アルバムを開く処理を呼び出す
+   @IBAction func UploadImageButton(_ sender: UIButton) {
+        // カメラロールが利用可能か？
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            // 写真を選ぶビュー
+            let pickerView = UIImagePickerController()
+            // 写真の選択元をカメラロールにする
+            // 「.camera」にすればカメラを起動できる
+            pickerView.sourceType = .photoLibrary
+            // デリゲート
+            pickerView.delegate = self
+            // ビューに表示
+            self.present(pickerView, animated: true)
+        }
+   /*     let picker = UIImagePickerController() //アルバムを開く処理を呼び出す
             picker.sourceType = .photoLibrary
             picker.delegate = self
             present(picker, animated: true)
             self.present(picker, animated: true, completion: nil)
+ */
     }
+    
+    
     /*
     // MARK: - Navigation
 
@@ -53,5 +72,15 @@ class ImageUploadViewController: UIViewController, UIImagePickerControllerDelega
 
 }
 
-
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // 写真を選んだ後に呼ばれる処理
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 選択した写真を取得する
+        let image = info[.originalImage] as! UIImage
+        // ビューに表示する
+        UploadImageView.image = image
+        // 写真を選ぶビューを引っ込める
+        self.dismiss(animated: true)
+    }
+}
 
