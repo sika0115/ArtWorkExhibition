e8cf52af7d3da854b12d1fd3f2cb7652ec2fc0fe
sika0115 <nh3988@gmail.com>
2020-11-20 12:16:29 +0900

    展示名(文字列:配列ではない)をDBに追加、VierControllerを複数追加

--- ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/ExhibitionNameViewController.swift
+++ ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/ExhibitionNameViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ImageExplanationUploadViewController.swift
+//  ExhibitionNameViewController.swift
 //  ArtworkExhibition
 //
-//  Created by 庄司果鈴 on 2020/11/13.
+//  Created by 庄司果鈴 on 2020/11/20.
 //  Copyright © 2020 Karin Shoji. All rights reserved.
 //
 
@@ -10,31 +10,30 @@ import UIKit
 import Firebase
 import FirebaseFirestore
 
-//作品名をDBにアップロード
-class ImageExplanationUploadViewController: UIViewController {
+//展示名をDBにアップロード
+class ExhibitionNameViewController: UIViewController {
 
-    @IBOutlet weak var AddImageExp: UITextField!
-    
-    // 文字列保存用の変数
-    var textFieldString = ""
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var ExibiNameField: UITextField!
     
-    @IBAction func UploadImageExp(_ sender: Any) {
+    //文字列保存用の変数
+    var exhibiNameString = ""
+    
+    @IBAction func UploadExibiName(_ sender: Any) {
         // TextField(AddImageExp)から文字を取得
-        textFieldString = AddImageExp.text!
+        exhibiNameString = ExibiNameField.text!
         // TextField(AddImageExp)の中身をクリア
-        AddImageExp.text = ""
+        ExibiNameField.text = ""
         
         //Cloud Firestoreに入力情報をアップロード、保存する
         let db = Firestore.firestore()
         db.collection("artworks")//コレクションにアクセス
         db.collection("artworks").document("artwork01")//データにアクセス
-        let data: [String: Any] = ["artwork01name":FieldValue.arrayUnion([textFieldString])] //更新用データ
+        let data: [String: Any] = ["exibiname":exhibiNameString] //更新用データ
         db.collection("artworks").document("artwork01").setData(data, merge: true)
     }
     
