feffc88f2079b93b8672dcedbf322eda3116d3db
sika0115 <nh3988@gmail.com>
2020-11-25 13:37:26 +0900

    閲覧中UI及びShowArtworkを追加

---
+++ ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/ShowArtworkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowArtworkViewController.swift
+//  ArtworkExhibition
+//
+//  Created by 庄司果鈴 on 2020/11/25.
+//  Copyright © 2020 Karin Shoji. All rights reserved.
+//
+
+import UIKit
+
+class ShowArtworkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
