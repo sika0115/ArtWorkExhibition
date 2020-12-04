45fbc97903b2910587a1b8453167b7b32ee85fcd
sika0115 <nh3988@gmail.com>
2020-12-04 15:21:36 +0900

    利用規約への同意アラート作成

--- ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/IntroViewController.swift
+++ ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/IntroViewController.swift
@@ -11,7 +11,21 @@ import UIKit
 class IntroViewController: UIViewController {
 
     @IBAction func UserRegistrationButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "アプリの使用に関する注意点", message: "他人の作品を無断でアップロードするのは禁止です。アプリ利用においてアップロードした画像は保存・取得以外の用途では用いません。", preferredStyle: UIAlertController.Style.alert)
+        let action1 = UIAlertAction(title: "同意", style: UIAlertAction.Style.default, handler: {(action: UIAlertAction!)in
+            print ("同意しました。")
+            //登録画面へ
+            self.performSegue(withIdentifier: "goSignUp", sender: nil)
+        })
+        let cancel = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler: {(action: UIAlertAction!)in
+            print("キャンセル")
+        })
+        
+        alert.addAction(action1)
+        alert.addAction(cancel)
+        self.present(alert, animated: true, completion: nil)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
