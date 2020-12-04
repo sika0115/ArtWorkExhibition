aaeb05df37dc3d8c9b8c076ae5f8549ed0c63361
sika0115 <nh3988@gmail.com>
2020-12-04 16:27:06 +0900

    そのユーザが作成した展示をマイページに表示

--- ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/MyPageViewController.swift
+++ ArtworkExhibition/ArtworkExhibition/ArtworkExhibition/MyPageViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseStorage
 
 class MyPageViewController: UIViewController {
 
@@ -25,7 +26,46 @@ class MyPageViewController: UIViewController {
             Username = username!
             print(username!)
         }
-        UsernameLabel.text = Username
+        UsernameLabel.text = Username //現在のユーザ表示
+        
+        func getImageByUrl(url: String) -> UIImage{
+            let url = URL(string: url)
+            do {
+                let data = try Data(contentsOf: url!)
+                return UIImage(data: data)!
+            } catch let err {
+                print("Error : \(err.localizedDescription)")
+            }
+            return UIImage()
+        }
+        
+        var thum_url = ""
+        var e_user = ""
+        
+        let db = Firestore.firestore()
+        let dbRef = db.collection("artworks").document("artwork01")
+        dbRef.getDocument{ (document, error) in
+                if let document = document {
+                    //データベースのデータを全て取得
+                    let data = document.data()
+                    //サムネのurl用変数に画像のurlを代入(dataからサムネのurlだけ抜き出す)
+                    thum_url = data!["thumbnail"] as! String
+                    e_user = data!["e_username"] as! String
+                    
+                    if e_user == self.Username {
+                        //取得したurlを変数に
+                        let imageFileUrl = thum_url
+                        //UIImageViewに表示するデータを準備
+                        let urlimage:UIImage = getImageByUrl(url: imageFileUrl)
+                        //UIImageViewにurlimageを指定して表示
+                        self.ImageView.image = urlimage
+                    } else {
+                        print("展示はありません")
+                    }
+                }else{
+                    print("Document does not exist")
+                }
+        }
         // Do any additional setup after loading the view.
     }
     
